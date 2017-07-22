4e9e1e8c050c30479f60442bca4be55b7cafb090
rohitas_b <rohitas_b@MGTS-IT-04>
2017-07-22 14:26:17 +0530

    firrst

---
+++ RedisConnectionTest/RedisConnectorHelper.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisConnectionTest
+{
+    public class RedisConnectorHelper
+    {
+        static RedisConnectorHelper()
+        {
+            RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                ConfigurationOptions cfo = new ConfigurationOptions();
+                string configuration   = "redis-18731.c13.us-east-1-3.ec2.cloud.redislabs.com:18731";   //localhost
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
